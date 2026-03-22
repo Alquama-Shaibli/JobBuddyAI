d498bae67eefba8a099717d0a202c29ba2782755
Jha-Shiva <shivamjha625@gmail.com>
2026-03-22 17:54:23 +0530

    added new field

--- server/models/job.model.js
+++ server/models/job.model.js
@@ -29,13 +29,31 @@ const jobSchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
+    experienceLevel:{
+        type: String,
+        enum: ['fresher', 'junior', 'mid', 'senior'],
+        default: 'fresher'
+    },
     salary:{
-        type: mongoose.Schema.Types.Mixed,
+        type: String,
         required: true,
         default:'Not mentioned'
+    },
+    jobType:{
+        type: String,
+        enum: ['full-time', 'part-time', 'remote', 'contract', 'internship'],
+        default: 'full-time'
+    },
+    postedBy:{
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true
     }
 },{ timestamps: true });
 
-const Job = mongoose.model('Job', jobSchema);
+// indexing helpful for future search
+// jobSchema.index({ title: "text", description: "text", company: "text" });
+
+const Job = mongoose.models.Job || mongoose.model('Job', jobSchema); //check if job model already exist then use that else make new model
 
 export default Job;
