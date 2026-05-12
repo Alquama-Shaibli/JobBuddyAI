3a8721923dfa3fcc12f82701f835bcebc2938b62
Jha-Shiva <shivamjha625@gmail.com>
2026-05-12 21:56:29 +0530

    submitMockTest functionality added to mockTest controller

--- server/controllers/mockTest.controllers.js
+++ server/controllers/mockTest.controllers.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import MockTest from '../models/mockTest.model.js';
+import Result from '../models/result.model.js';
+
 export const createMockTest = async (req, res, next)=>{
     const { title, category, questions, timeLimit } = req.body;
 
@@ -71,6 +73,61 @@ export const getMockTestbyId = async (req, res, next) => {
             message: "success",
             mocktest
         });
+    } catch (error) {
+        next(error)
+    }
+};
+
+export const submitMockTest = async (req, res, next) => {
+    const mockTestId = req.params.id;
+    const { answers } = req.body;
+
+    if(!req.user){
+        return next({statusCode: 403, message: "You are not authorized !!!"});
+    };
+
+    if(!mongoose.Types.ObjectId.isValid(mockTestId)){
+        return next({statusCode: 403, message: "Invalid mocktest Id"});
+    };
+
+    try {
+        const mockTest = await MockTest.findById(mockTestId);
+
+        let score = 0;
+
+        const resultAnswers = [];
+
+        mockTest.questions.forEach((question)=>{
+            const userAnswer = answers.find((ans)=> ans.questionId === question._id.toString());
+
+            const isCorrect = userAnswer?.selectedAnswer === question.correctAnswer;
+
+            if(isCorrect) {
+                score ++;
+            };
+
+            resultAnswers.push({
+                questionId: question._id,
+                selectedAnswer: userAnswer?.selectedAnswer || "",
+                correctAnswer: question.correctAnswer
+            });
+
+        });
+
+        // saving result
+        const result = await Result.create({
+            userId: req.user.id,
+            testId: mockTestId,
+            score,
+            totalQuestion: mockTest.questions.length,
+            answers: resultAnswers
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "MockTest Submitted Successfully",
+            result
+        })
     } catch (error) {
         next(error)
     }
--- server/routes/mockTest.routes.js
+++ server/routes/mockTest.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createMockTest, getAllMockTest, getMockTestbyId } from '../controllers/mockTest.controllers.js';
+import { createMockTest, getAllMockTest, getMockTestbyId, submitMockTest } from '../controllers/mockTest.controllers.js';
 import verifyToken from '../middlewares/verifyToken.js';
 
 const router = express.Router();
 
+router.post('/create',verifyToken, createMockTest);
 router.get('/', verifyToken, getAllMockTest)
 router.get('/:id', verifyToken, getMockTestbyId)
-router.post('/create',verifyToken, createMockTest);
+router.post('/submit/:id', verifyToken, submitMockTest)
 
 export default router;
